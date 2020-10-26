22adf45d0a0c3765ec06a8771f5a0ff7cfa04481
metrio <dlima.nyit@gmail.com>
2020-10-26 18:56:08 -0400

    commit tree 4ae853ac80ae6208559b0c33fb016fdfe0df6e8f

--- app/application.rb
+++ app/application.rb
@@ -15,17 +15,17 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/car/)
+    elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
       else 
         @@cart.each do |item|
-          resp.write"#{item}\n"
+          resp.write "#{item}\n"
         end
       end
 
     elsif req.path.match(/add/)
-      item_to_add = req.params["items"]
+      item_to_add = req.params["item"]
       if @@items.include? item_to_add
         @@cart << item_to_add
         resp.write "added #{item_to_add}"
